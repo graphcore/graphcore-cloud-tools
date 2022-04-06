d63cc83d51a56c91f38c518d0965c004441c71b4
Blazej Banaszewski <102740547+blazejba@users.noreply.github.com>
2022-04-06 17:51:42 +0100

    Fix file argparse test (#5)
    * fix argparse test
    
    * make lint
    
    * Update file_argparse.py

--- tests/test_file_argparser.py
+++ tests/test_file_argparser.py
@@ -8,6 +8,8 @@ from examples_utils.parsing import file_argparse
 
 def add_arguments(parser):
     parser.add_argument('--config', type=str)
+    parser.add_argument('--config-path', type=str)
+    parser.add_argument('--model-name', type=str)
     return parser
 
 
@@ -15,6 +17,6 @@ class LoadingConfigsTest(unittest.TestCase):
     def test_loading_configs(self):
         parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser = add_arguments(parser)
-        args = parser.parse_args(['--config', 'resnet8_test'])
+        args = parser.parse_args(['--config', 'resnet8_test', '--config-path', 'tests/test_files/configs.yml'])
         args = file_argparse.parse_yaml_config(args, parser)
         assert args.model_name == 'cifar_resnet8'
