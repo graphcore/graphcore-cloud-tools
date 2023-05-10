8e4c443c762887e2b7c9d217bec8676ca040f76e
Hitesh Kumar <hiteshk@graphcore.ai>
2023-05-10 15:31:12 +0100

    adding manual termination check and fixes (#101)
    - Adding a notebook name field for cleaner data analysis in superset
    - Manual cell execution termination event checking added
    - Adding checks if event was logging termination
    - Adding checks if event was compilation based on checking input/outputs
    (WIP)
    - Detecting compile times (WIP)
    
    Edit:
    - Removing compile time detection in this PR, will require some more
    work with poptorch possibly

--- examples_utils/notebook_logging/gc_logger.py
+++ examples_utils/notebook_logging/gc_logger.py
@@ -9,6 +9,7 @@ import os
 import pkg_resources
 import time
 import multiprocessing as mp
+import nbformat
 
 from datetime import datetime
 from pathlib import Path
@@ -26,26 +27,46 @@ class GCLogger(object):
     _POLLING_SECONDS = 10
 
     _MP_MANAGER = mp.Manager()
-
     _PAYLOAD = _MP_MANAGER.dict()
+    _CODE_CELLS = _MP_MANAGER.list()
 
     _PROC_LIST = []
 
     _FIREHOSE_STREAM_NAME = os.getenv("FIREHOSE_STREAM_NAME", "paperspacenotebook_production")
     _REGION = "eu-west-1"
 
-    _FRAMEWORKS = ["poptorch", "torch", "transformers", "tensorflow", "poptorch-geometric"]
+    _FRAMEWORKS = [
+        "poptorch",
+        "torch",
+        "transformers",
+        "tensorflow",
+        "poptorch-geometric",
+    ]
 
     _COLUMN_TYPES = {
+        # Timing data
         "event_time": "",
         "execution_start_time": "",
         "execution_end_time": "",
+        "time_to_first_error_seconds": 0,
+        "compile_time_seconds": 0,
+        # Event metadata
         "event_type": "",
         "user_onetime_id": "",
+        "manual_logging_termination_event": 0,
+        "manual_cell_termination_event": 0,
+        # Largely constant values
         "notebook_path": "",
         "notebook_repo_id": "",
         "notebook_id": "",
         "cluster_id": "",
+        "repo_framework": "",
+        # Cell input/output information
+        "error_trace": "",
+        "cell_output": "",
+        "code_executed": "",
+        "cell_code_modified": 0,
+        # Major framework versions from env
         "poptorch_version_major": 0,
         "poptorch_version_minor": 0,
         "poptorch_version_patch": "",
@@ -61,10 +82,6 @@ class GCLogger(object):
         "popgeometric_version_major": 0,
         "popgeometric_version_minor": 0,
         "popgeometric_version_patch": "",
-        "time_to_first_error_seconds": 0,
-        "error_trace": "",
-        "cell_output": "",
-        "code_executed": "",
     }
 
     _HF_KEY_LENGTH = 37
@@ -123,6 +140,7 @@ class GCLogger(object):
                 background_functions = [
                     cls.__get_notebook_metadata,
                     cls.__get_frameworks_versions,
+                    cls.__store_initial_cell_states,
                 ]
 
                 # Start multiprocess procs for all functions
@@ -140,7 +158,7 @@ class GCLogger(object):
         return
 
     @classmethod
-    def __update_payload(cls, output: str, name: str) -> str:
+    def __update_payload(cls, output: str or int, name: str) -> str:
         """Update the payload with empty types as backups."""
 
         if cls.LOG_STATE == "DISABLED":
@@ -153,37 +171,64 @@ class GCLogger(object):
             cls._PAYLOAD[name] = empty_output_type()
 
     @classmethod
-    def __get_notebook_metadata(cls):
-        """Get notebook metadata."""
+    def __store_initial_cell_states(cls):
+        """store the initial state of all cells in notebook."""
+
+        if cls.LOG_STATE == "DISABLED":
+            return
+
+        try:
+            with open(ipynbname.path()) as notebook:
+                initial_state = nbformat.read(notebook, nbformat.NO_CONVERT)
+
+            # Get list of all code cells
+            for cell in initial_state["cells"]:
+                if cell["cell_type"] == "code":
+                    cls._CODE_CELLS.append(cell["source"])
+
+        except:
+            pass
+
+    @classmethod
+    def __manual_termination_polling(cls):
+        """Report if exeuction termination event was some kill signal."""
 
         try:
             while True:
-                if cls.LOG_STATE == "DISABLED":
-                    return
-
-                try:
-                    notebook_path = str(ipynbname.path())
-                except:
-                    notebook_path = "failed-to-get-nb-path"
-
-                # Encode and hash
-                notebook_id = os.getenv("PAPERSPACE_NOTEBOOK_ID")
-                salted_id = notebook_id + datetime.now().strftime("%Y-%m-%d")
-                anonymised_notebook_id = base64.urlsafe_b64encode(
-                    hashlib.md5(salted_id.encode("utf-8")).digest()
-                ).decode("ascii")[:16]
-
-                notebook_metadata = {
-                    "notebook_path": notebook_path,
-                    "notebook_repo_id": os.getenv("PAPERSPACE_NOTEBOOK_REPO_ID"),
-                    "notebook_id": anonymised_notebook_id,
-                    "cluster_id": os.getenv("PAPERSPACE_CLUSTER_ID"),
-                }
-
-                for key, val in notebook_metadata.items():
-                    cls.__update_payload(val, key)
-
                 time.sleep(cls._POLLING_SECONDS)
+        except:
+            cls.__update_payload(1, "manual_cell_termination_event")
+
+    @classmethod
+    def __get_notebook_metadata(cls):
+        """Get notebook metadata."""
+
+        if cls.LOG_STATE == "DISABLED":
+            return
+
+        try:
+            try:
+                notebook_path = ipynbname.path()
+            except:
+                notebook_path = Path("failed-to-get-nb-path")
+
+            # Encode and hash
+            notebook_id = os.getenv("PAPERSPACE_NOTEBOOK_ID")
+            salted_id = notebook_id + datetime.now().strftime("%Y-%m-%d")
+            anonymised_notebook_id = base64.urlsafe_b64encode(hashlib.md5(salted_id.encode("utf-8")).digest()).decode(
+                "ascii"
+            )[:16]
+
+            notebook_metadata = {
+                "notebook_path": str(notebook_path),
+                "notebook_repo_id": os.getenv("PAPERSPACE_NOTEBOOK_REPO_ID"),
+                "notebook_id": anonymised_notebook_id,
+                "cluster_id": os.getenv("PAPERSPACE_CLUSTER_ID"),
+                "repo_framework": os.getenv("REPO_FRAMEWORK"),
+            }
+
+            for key, val in notebook_metadata.items():
+                cls.__update_payload(val, key)
 
         except:
             pass
@@ -192,32 +237,90 @@ class GCLogger(object):
     def __get_frameworks_versions(cls) -> str:
         """Get framework versions."""
 
+        if cls.LOG_STATE == "DISABLED":
+            return
+
         try:
-            while True:
-                if cls.LOG_STATE == "DISABLED":
-                    return
+            try:
+                installed_packages = pkg_resources.working_set
+            except:
+                installed_packages = {}
 
-                try:
-                    installed_packages = pkg_resources.working_set
-                except:
-                    installed_packages = {}
+            # Query pip packages and versions for frameworks
+            all_pkgs = {i.key: i.version for i in installed_packages}
+            for fw in cls._FRAMEWORKS:
+                version = all_pkgs.get(fw, "..").split(".")
 
-                # Query pip packages and versions for frameworks
-                all_pkgs = {i.key: i.version for i in installed_packages}
-                for fw in cls._FRAMEWORKS:
-                    version = all_pkgs.get(fw, "..").split(".")
+                if fw == "poptorch-geometric":
+                    fw = "popgeometric"
 
-                    if fw == "poptorch-geometric":
-                        fw = "popgeometric"
-
-                    cls.__update_payload(int(version[0]) if version[0] else 0, f"{fw}_version_major")
-                    cls.__update_payload(int(version[1]) if version[0] else 0, f"{fw}_version_minor")
-                    cls.__update_payload(version[2], f"{fw}_version_patch")
-
-                time.sleep(cls._POLLING_SECONDS)
+                cls.__update_payload(int(version[0]) if version[0] else 0, f"{fw}_version_major")
+                cls.__update_payload(int(version[1]) if version[0] else 0, f"{fw}_version_minor")
+                cls.__update_payload(version[2], f"{fw}_version_patch")
 
         except:
             pass
+
+    @classmethod
+    def __convert_time_from_string(cls, raw_string_time: str) -> int:
+        """Convert times from MM:SS string format to integer seconds"""
+
+        minutes = int(raw_string_time[:2])
+        seconds = int(raw_string_time[3:])
+
+        return (minutes * 60) + seconds
+
+    @classmethod
+    def __get_compile_time(cls, cell_input: str, cell_output: str) -> int:
+        """Capture compile time from a cells inputs/outputs."""
+
+        if cls.LOG_STATE == "DISABLED":
+            return
+
+        # Whether any compil/e/ation happened or not
+        if not "compil" in cell_input + cell_output:
+            # Covers most HF, PyG and Pytorch cases
+            if "Graph compilation: 100%" in cell_output:
+                start_index = cell_output.find("Graph compilation: 100%")
+                end_index = cell_output.find("00:00]")
+                compile_time_raw = cell_output[start_index:end_index][-6:-1]
+                compile_time = cls.__convert_time_from_string(compile_time_raw)
+            else:
+                compile_time = 0
+
+        return compile_time
+
+    @classmethod
+    def __detect_logging_termination(cls, cell_input: str) -> int:
+        """Detect if GCL logging was terminated by user"""
+
+        if cls.LOG_STATE == "DISABLED":
+            return
+
+        if "unload_ext gc_logger" in cell_input:
+            return 1
+        else:
+            return 0
+
+    @classmethod
+    def __detect_cell_modification(cls, executed_code: str) -> int:
+        """Detect if the cell code has been modified before execution."""
+
+        if cls.LOG_STATE == "DISABLED":
+            return
+
+        if cls._CODE_CELLS == []:
+            return 0
+
+        try:
+            if executed_code in cls._CODE_CELLS:
+                return 0
+            else:
+                return 1
+        except:
+            pass
+
+        return 0
 
     @classmethod
     def __remove_hf_keys(cls, raw_string: str) -> str:
@@ -234,14 +337,14 @@ class GCLogger(object):
         return raw_string
 
     @classmethod
-    def __sanitize_payload(cls, payload):
+    def __sanitize_payload(cls, payload: dict) -> dict:
 
         if cls.LOG_STATE == "DISABLED":
             return
 
         # Clean out any private keys, fix quotes
         for key, val in payload.items():
-            if type(val) == str:
+            if (val is not None) and (type(val) == str):
                 if key in ["error_trace", "cell_output", "code_executed"]:
                     val = cls.__remove_hf_keys(val)
 
@@ -253,7 +356,7 @@ class GCLogger(object):
         return payload
 
     @classmethod
-    def __firehose_put(cls, payload):
+    def __firehose_put(cls, payload: dict):
         """Submit a PUT record request to the firehose stream."""
 
         if cls.LOG_STATE == "DISABLED":
@@ -291,6 +394,16 @@ class GCLogger(object):
         event_dict["code_executed"] = str(result.info.raw_cell)
         event_dict["cell_output"] = str(result.result)
 
+        # Get compile time if available
+        event_dict["compile_time_seconds"] = cls.__get_compile_time(
+            event_dict["code_executed"],
+            event_dict["cell_output"],
+        )
+
+        # Detect if this cell is new or has been modified from its initial state
+        # TODO: Once we upgrade to newer IPython, we can distinguish these two
+        event_dict["cell_code_modified"] = cls.__detect_cell_modification(result.info.raw_cell)
+
         if result.error_before_exec or result.error_in_exec:
             # Only get this value once
             if cls._PAYLOAD["time_to_first_error_seconds"] == 0:
@@ -304,17 +417,28 @@ class GCLogger(object):
             event_dict["event_type"] = "success"
             event_dict["error_trace"] = ""
 
+        event_dict["manual_logging_termination_event"] = cls.__detect_logging_termination(result.info.raw_cell)
+
         cls.__firehose_put(event_dict)
 
 
 def load_ipython_extension(ip):
+    """Instructions for loading the extension."""
+
     global _gc_logger
     _gc_logger = GCLogger(ip)
+
     ip.events.register("pre_run_cell", _gc_logger.pre_run_cell)
     ip.events.register("post_run_cell", _gc_logger.post_run_cell)
 
 
 def unload_ipython_extension(ip):
+    """Instructions for unloading the extension."""
+
     global _gc_logger
     _gc_logger.LOG_STATE = "DISABLED"
+
+    ip.events.unregister("pre_run_cell", _gc_logger.pre_run_cell)
+    ip.events.unregister("post_run_cell", _gc_logger.post_run_cell)
+
     del _gc_logger
