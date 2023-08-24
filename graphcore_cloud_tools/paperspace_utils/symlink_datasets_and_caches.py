753cc8daf11366b84207a59dc133633ec7b4df31
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2023-08-24 13:47:59 +0100

    Check symlink worked (#13)
    Adds self-checks when running the symlink:
    - Check for exit code working
    - Check for file consistency before after execution
    
    Also adds tests for the bugs that were encountered.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: payoto <payoto@users.noreply.github.com>

--- graphcore_cloud_tools/paperspace_utils/symlink_datasets_and_caches.py
+++ graphcore_cloud_tools/paperspace_utils/symlink_datasets_and_caches.py
@@ -34,27 +34,27 @@ def check_dataset_is_mounted(source_dirs_list: List[str]) -> List[str]:
     return source_dirs_exist_paths
 
 
-def create_overlays(source_dirs_exist_paths: List[str], target_dir: str) -> None:
+def create_overlays(source_dirs_exist_paths: List[str], target_dir: str) -> subprocess.CompletedProcess:
     print(f"Symlinking - {source_dirs_exist_paths} to {target_dir}")
     print("-" * 100)
 
     Path(target_dir).mkdir(parents=True, exist_ok=True)
 
-    workdir = Path(FUSEOVERLAY_ROOT) / "workdirs" / source_dirs_exist_paths[0]
+    # Use this path construction as pathlib resolves 'path1 / "/path"' -> "/path"
+    workdir = Path(FUSEOVERLAY_ROOT) / f"workdirs/{source_dirs_exist_paths[0]}"
     workdir.mkdir(parents=True, exist_ok=True)
-    upperdir = Path(FUSEOVERLAY_ROOT) / "upperdir" / source_dirs_exist_paths[0]
+    upperdir = Path(FUSEOVERLAY_ROOT) / f"upperdirs/{source_dirs_exist_paths[0]}"
     upperdir.mkdir(parents=True, exist_ok=True)
 
     lowerdirs = ":".join(source_dirs_exist_paths)
     overlay_command = f"fuse-overlayfs -o lowerdir={lowerdirs},upperdir={upperdir.as_posix()},workdir={workdir.as_posix()} {target_dir}"
-    subprocess.run(
+    out = subprocess.run(
         overlay_command.split(),
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
     )
-
-    return
+    return out
 
 
 def parse_symlinks_args(parser: argparse.ArgumentParser):
@@ -71,13 +71,38 @@ def run_symlinks(args):
 
     # loop through each key-value pair
     # the key is the target directory, the value is a list of source directories
+    symlink_results = []
+    expected_paths = []
+    found_target_paths = []
     for target_dir, source_dirs_list in config.items():
         # need to wait until the dataset has been mounted (async on Paperspace's end)
         source_dirs_exist_paths = check_dataset_is_mounted(source_dirs_list)
 
         # create overlays for source dataset dirs that are mounted and populated
+        out = f"There were no source directories mounted from {source_dirs_list}"
+        # add all the files detected in a source dir, to be expected after symlinking
+        target_path = Path(target_dir).resolve()
+        for source_path in source_dirs_exist_paths:
+            source_path = Path(source_path).resolve()
+            expected_paths.extend(
+                [str((target_path / f.relative_to(source_path)).resolve()) for f in source_path.rglob("*")]
+            )
         if len(source_dirs_exist_paths) > 0:
-            create_overlays(source_dirs_exist_paths, target_dir)
+            out = create_overlays(source_dirs_exist_paths, target_dir)
+        symlink_results.append((target_dir, out))
+        found_target_paths.extend([str(f) for f in target_path.rglob("*")])
+    errors = [f"{t} failed with error: {o}" for t, o in symlink_results if isinstance(o, str) or o.returncode != 0]
+    if errors:
+        raise RuntimeError("\n".join(errors))
+    missing_files = [e for e in expected_paths if e not in found_target_paths]
+    if missing_files:
+        raise FileNotFoundError(
+            "The symlink config was not applied correctly, some files could not be found in their expected location.\n"
+            f"Config: {config}\n"
+            f"missing_files: {missing_files}\n"
+            f"expected_paths: {expected_paths}\n"
+            f"found_target_paths: {found_target_paths}\n"
+        )
 
 
 if __name__ == "__main__":
