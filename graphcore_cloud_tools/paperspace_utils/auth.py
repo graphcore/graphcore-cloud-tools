af821ae4e24ccc501a88f57fa3e7153181510113
Alexandre Payot <alexandrep@graphcore.ai>
2023-09-11 16:12:19 +0100

    Add comment

--- graphcore_cloud_tools/paperspace_utils/auth.py
+++ graphcore_cloud_tools/paperspace_utils/auth.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 
-AWS_CREDENTIAL_ENV_VAR = "DATASET_S3_DOWNLOAD_B64_CREDENTIAL"  # See confluence
+# Internal documentation available here:
+# https://graphcore.atlassian.net/wiki/spaces/PM/pages/3323920580/Credential+handling+for+S3+Paperspace+production+service
+AWS_CREDENTIAL_ENV_VAR = "DATASET_S3_DOWNLOAD_B64_CREDENTIAL"
 DEFAULT_S3_CREDENTIAL = """W2djZGF0YS1yXQphd3NfYWNjZXNzX2tleV9pZCA9IDc0Q0QwUVVHVkEwUVo3WUZSSlhSCmF3c19zZWNyZXRf
 YWNjZXNzX2tleSA9IExDZENYMEs1aW1USUZRTkVZQzVnY3VkT2prWlFmcHkxZ0p4VjN1RkwK"""
 
